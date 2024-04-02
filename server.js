34f5c74b34a4d41238e89dc968fe894e2dca7d1f
unknown <pyke22154041e@gmail.com>
2024-04-02 08:28:17 +0700

    b

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+let {run_db_server} = require('./lib/config/mongodb');
+const globalErrorHandler = require('./lib/middlewares/handlingGlobalError');
+const PORT = process.env.PORT || 9000;
+const subjectRoutes = require('./lib/routes/subject/index');
+const testRoutes = require('./lib/routes/tests/index');
+run_db_server();
+
+app.use(express.json());
+app.use("/api/v1/subjects", subjectRoutes);
+app.use("/api/v1/tests", testRoutes);
+
+
+
+app.use((err, req, res, next) =>{
+    // stack
+    globalErrorHandler(err, req, res, next);
+});
+app.listen(PORT, console.log(`Server is up and running on ${PORT}`));
